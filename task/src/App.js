ef3070d260778bc75a19f8f1a182eb65a53389d3
minaz99 <hanymina43@gmail.com>
2022-11-26 16:58:23 +0100

    Set up React app + TailwindCSS

---
+++ task/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-amber-200 h-screen">
+      <h1 className="text-violet-300 text-2xl text-center">
+        Enter a number between 5-20
+      </h1>
+    </div>
+  );
+}
+
+export default App;
